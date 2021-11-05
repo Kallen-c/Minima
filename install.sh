be6fa59c1fec2dd468fde658f263c6975542e668
Kallen-c <kallenc@example.org>
2021-11-05 21:29:02 +0300

    fix

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 echo -e "\033[35m"
-echo -e "https://t.me/ro_cryptoo"
-echo -e "https://t.me/whitelistx1000"
+
 
 echo -ne "\033[35m██████╗░░█████╗░  "
 echo -e "\033[34m░█████╗░██████╗░██╗░░░██╗██████╗░████████╗░█████╗░"
@@ -22,30 +21,29 @@ echo -ne "\033[35m╚═╝░░╚═╝░╚════╝░  "
 echo -e "\033[34m░╚════╝░╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░░░░░░░╚═╝░░░░╚════╝░"
 
 echo -e "\033[35m"
+echo -e "-----------------------------------------------------------------------"
+echo -e "https://t.me/ro_cryptoo"
+echo -e "https://t.me/whitelistx1000"
+echo -e "-----------------------------------------------------------------------"
 
 echo -e "Installing..."
-sudo apt -qq update
-sudo apt -qq install ufw -y
-sudo ufw allow 22:65535/tcp
-sudo ufw allow 22:65535/udp
-sudo ufw deny out from any to 10.0.0.0/8
-sudo ufw deny out from any to 192.168.0.0/16
-sudo ufw deny out from any to 100.64.0.0/10
-sudo ufw deny out from any to 198.18.0.0/15
-sudo ufw deny out from any to 169.254.0.0/16
+sudo apt  update > /dev/null
+sudo apt  install ufw -y > /dev/null
+sudo ufw allow 22:65535/tcp > /dev/null
+sudo ufw allow 22:65535/udp > /dev/null
+sudo ufw deny out from any to 10.0.0.0/8 > /dev/null
+sudo ufw deny out from any to 192.168.0.0/16 > /dev/null
+sudo ufw deny out from any to 100.64.0.0/10 > /dev/null
+sudo ufw deny out from any to 198.18.0.0/15 > /dev/null
+sudo ufw deny out from any to 169.254.0.0/16 > /dev/null
 sudo ufw --force enable
 
 echo -e "\033[35m"
-apt -qq install mc wget jq libfontconfig1 libxtst6 libxrender1 libxi6 java-common -y
-echo -e "\033[35m"
-wget -qq https://cdn.azul.com/zulu/bin/zulu11.48.21-ca-jdk11.0.11-linux_amd64.deb
-echo -e "\033[35m"
-dpkg -i zulu11.48.21-ca-jdk11.0.11-linux_amd64.deb
-echo -e "\033[35m"
-wget https://github.com/minima-global/Minima/raw/master/jar/minima.jar
-echo -e "\033[35m"
+apt  install mc wget jq libfontconfig1 libxtst6 libxrender1 libxi6 java-common -y > /dev/null
+wget -q  https://cdn.azul.com/zulu/bin/zulu11.48.21-ca-jdk11.0.11-linux_amd64.deb
+dpkg -i zulu11.48.21-ca-jdk11.0.11-linux_amd64.deb > /dev/null
+wget -q https://github.com/minima-global/Minima/raw/master/jar/minima.jar
 sudo apt -qq  install --fix-broken -y
-echo -e "\033[35m"
 sudo tee <<EOF >/dev/null /etc/systemd/journald.conf
 Storage=persistent
 EOF
@@ -69,6 +67,7 @@ sudo systemctl start minima
 . <(wget -qO- https://raw.githubusercontent.com/Kallen-c/utils/main/miscellaneous/insert_variable.sh) -n minima_log -v "sudo journalctl -f -n 100 -u minima" -a
 
 
+echo -e "Installing completed, check logs"
 echo -e "\033[0m"
 
 echo -e '\e[0m\n'
